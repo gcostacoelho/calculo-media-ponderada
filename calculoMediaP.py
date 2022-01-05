0195ea91ff221a0af097fb4b3208f290356bd185
gcostacoelho <gcostacoelho2003@gmail.com>
2022-01-05 13:16:41 -0300

    second-commit

--- calculoMediaP.py
+++ calculoMediaP.py
@@ -10,26 +10,6 @@ def recebeNotas():
         qtdeNota = int(input('Deseja adicionar quantas notas?\nR: '))
         
         for i in range(qtdeNota):
-            notas.append(float(input(f'Nota {n}: ')))
-            pesos.append(float(input('Insira o peso dessa nota: ')))
-            n += 1
-        
-        maisNota = input('Deseja continuar adicionando notas?\nR: ').lower()
-    
-        if maisNota == 'nao':
-            print('Fazendo o cálculo da média')
-            break
-    return notas, pesos 
-
-def calculaMedia(notas, pesos):
-    for i in range(len(notas)):
-        notasPeso.append(notas[i] * pesos[i])
-def recebeNotas():
-    n = 1
-    while True:
-        qtdeNota = int(input('Deseja adicionar quantas notas?\nR: '))
-        
-        for i in range(qtdeNota):
             notas.append(int(input(f'Nota {n}: ')))
             pesos.append(float(input('Insira o peso dessa nota: ')))
             n += 1
@@ -56,19 +36,6 @@ def exibeNotas(media):
         n += 1
 
     print('Média aritmética = %.2f' % media)
-
-    media = sum(notasPeso)/sum(pesos)
-    return media
-
-def exibeNotas(media):
-    n = 1
-    for i in range(len(notas)):
-        print(f'Nota {n} = {notas[i]}')
-        print(f'Peso {n} = {pesos[i]}\n')
-        n += 1
-
-    print('Média aritmética = %.2f' % media)
-
 
 recebeNotas()
 media = calculaMedia(notas, pesos)
